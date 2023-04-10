3c6bedb29b39a9eb7ab1eaef5ae3a0993507b35a
Hannah6898 <hannahosibodu68@outlook.com>
2023-04-10 13:54:17 +0100

    Added JSX and CSS for User card

--- src/user/pages/Users.js
+++ src/user/pages/Users.js
@@ -1,5 +1,17 @@
+import UsersList from "../components/UsersList";
+
+const USERS = [
+  {
+    id: 1,
+    name: "Hannah Osibodu",
+    image:
+      "https://images.unsplash.com/photo-1500916434205-0c77489c6cf7?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+    places: 3,
+  },
+];
+
 function Users() {
-    return ( <h2>Users</h2> );
+  return <UsersList items={USERS} />;
 }
 
 export default Users;
