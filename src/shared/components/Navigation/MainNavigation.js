3cd93ec26a722b122301884d11e58cc802d95513
Hannah6898 <hannahosibodu68@outlook.com>
2023-04-12 08:28:37 +0100

    Added Nav Links to header

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,12 +1,13 @@
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
 import {Link} from 'react-router-dom'
+import NavLinks from "./NavLinks";
 
 function MainNavigation() {
   return (
     <MainHeader>
       <h1 className="main-navigation__title"> <Link to="/">YourPlaces</Link></h1>
-      <nav>...</nav>
+      <nav><NavLinks/></nav>
       <button className="main-navigation__menu-btn">
         <span />
         <span />
--- src/shared/components/Navigation/NavLinks.js
+++ src/shared/components/Navigation/NavLinks.js
@@ -1,5 +1,21 @@
-function NavLinks() {
-    return ( <div></div> );
+import './NavLinks.css'
+import { NavLink } from 'react-router-dom';
+
+function NavLinks(props) {
+    return ( <ul className='nav-links'>
+        <li>
+            <NavLink to="/" exact>All Users</NavLink>
+        </li>
+        <li>
+            <NavLink to="/u1/places">My Places</NavLink>
+        </li>
+        <li>
+            <NavLink to="/places/new">New Place</NavLink>
+        </li>
+        <li>
+            <NavLink to="/login">Login</NavLink>
+        </li>
+    </ul>);
 }
 
 export default NavLinks;
