7f86c376f4731ba56354e1e1cd9d6291946b4a6a
Hannah6898 <hannahosibodu68@outlook.com>
2023-04-12 15:03:50 +0100

    Added the side drawer and added animation for the drawer

--- src/shared/components/Navigation/MainNavigation.js
+++ src/shared/components/Navigation/MainNavigation.js
@@ -1,19 +1,49 @@
+import React, { Fragment, useState } from "react";
 import "./MainNavigation.css";
 import MainHeader from "./MainHeader";
-import {Link} from 'react-router-dom'
+import { Link } from "react-router-dom";
 import NavLinks from "./NavLinks";
+import SideDrawer from "./SideDrawer";
+import Backdrop from "../UIElements/Backdrop";
 
 function MainNavigation() {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  //Opening and closing of the Side drawer 
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  };
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  };
+  //If the size of the page increases and the side drawer is open, it should automtically disappear
+  // if (window.matchMedia("(min-width: 768px)")){
+  //   setDrawerIsOpen(false)
+  // }
+
   return (
-    <MainHeader>
-      <h1 className="main-navigation__title"> <Link to="/">YourPlaces</Link></h1>
-      <nav><NavLinks/></nav>
-      <button className="main-navigation__menu-btn">
-        <span />
-        <span />
-        <span />
-      </button>
-    </MainHeader>
+    <Fragment>
+      {drawerIsOpen ? <Backdrop onClick={closeDrawerHandler} /> : null}
+      <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
+        <nav className="main-navigation__drawer-nav">
+          <NavLinks />
+        </nav>
+      </SideDrawer>
+      <MainHeader>
+        <h1 className="main-navigation__title">
+          {" "}
+          <Link to="/">YourPlaces</Link>
+        </h1>
+        <nav className="main-navigation__header-nav">
+          <NavLinks />
+        </nav>
+        <button className="main-navigation__menu-btn" onClick={openDrawerHandler}>
+          <span />
+          <span />
+          <span />
+        </button>
+      </MainHeader>
+    </Fragment>
   );
 }
 
